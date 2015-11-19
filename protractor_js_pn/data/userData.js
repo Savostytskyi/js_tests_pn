92050e731b865e88bfd34df75a2f7740abbca835
Anton Savostytskyi <Anton_savostytskyi@epam.com>
2015-11-19 19:22:23 +0200

    js pn v1.1.0

--- protractor_js_pn/data/userData.js
+++
@@ -1,6 +0,0 @@
-
-// store user data in maps for ease of use and readability...
-var UserData = function() {
-    this.testUser = {'username': 'test', 'password': 'test'};
-};
-module.exports = new UserData;
